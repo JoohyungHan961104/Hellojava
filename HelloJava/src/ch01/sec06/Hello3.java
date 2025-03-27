75ec00233c940cc21982dd8495828a9c215a8364
JoohyungHan961104 <joohyunghan1104@gmail.com>
2025-03-27 17:59:37 +0900

    hello

---
+++ HelloJava/src/ch01/sec06/Hello3.java
@@ -0,0 +1,13 @@
+package ch01.sec06;
+
+public class Hello3 {
+
+	public static void main(String[] args) {
+		int a;
+		a=1;
+		
+		System.out.println("안녕,자바");
+
+	}
+
+}
---
+++ HelloJava/src/ch01/sec06/StringExample.java
@@ -0,0 +1,15 @@
+package ch01.sec06;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String name = "한주형";
+		String job = "프로그래머";
+		System.out.println(name);
+		System.out.println(job);
+		
+		String str = "나는\"자바\"를 배웁니다.";
+		System.out.println(str);
+		
+	}
+}
